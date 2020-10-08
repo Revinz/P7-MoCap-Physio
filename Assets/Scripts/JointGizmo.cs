c1d8816eb4f52a3ae38b9aa656ccfd3a4d00deb5
Revinz <revinz1405@gmail.com>
2020-10-08 16:03:25 +0200

    Added Joint Gizmos to the Rokoko provided model

---
+++ Assets/Scripts/JointGizmo.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class JointGizmo : MonoBehaviour
+{
+    public float gizmoSphereSize = 0.07f;
+    public float gizmoLineLength = 0.2f;
+    public Vector3 rotOffset = Vector3.zero;
+    private Vector3 pos;
+    private Vector3 rot;
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        //Location
+        Gizmos.DrawSphere(gameObject.transform.position, gizmoSphereSize);
+
+        //Joint facing direction
+        Gizmos.DrawLine(gameObject.transform.position, gameObject.transform.position + gameObject.transform.forward.normalized * gizmoLineLength);
+
+    }
+}
