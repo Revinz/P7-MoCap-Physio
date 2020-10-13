a961753ad50a0702d8aee3cef404084bd9632ae8
Revinz <revinz1405@gmail.com>
2020-10-13 12:53:15 +0200

    Made the skeleton revolve around the hips at all times. Thus preventing location being an issue.

--- Assets/Scripts/JointScripts/Joint.cs
+++ Assets/Scripts/JointScripts/Joint.cs
@@ -2,6 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// WIP
+/// Doesn't work.
+/// </summary>
+
+
 public class Joint : MonoBehaviour
 {
     private Vector3 pos;
@@ -10,10 +16,24 @@ public class Joint : MonoBehaviour
     public Vector3 Position { get => pos; }
     public Vector3 Rotation { get => rot; }
 
-    // Update is called once per frame
-    void Update()
+    public Vector3 prevPosition;
+
+    private void Update()
     {
+        prevPosition = this.gameObject.transform.position;
+    }
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if (Application.isEditor)
+            return;
+
         pos = this.gameObject.transform.position;
         rot = this.gameObject.transform.rotation.eulerAngles;
+
+        if (Mathf.Abs(Vector3.Distance(prevPosition, pos)) > 0.05f) {
+            this.gameObject.transform.position = prevPosition;
+        }
+
     }
 }
