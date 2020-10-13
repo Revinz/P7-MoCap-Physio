9b48ade49095a3bbc8b2caaff7a2facdf56e1f4a
Revinz <revinz1405@gmail.com>
2020-10-13 10:55:07 +0200

    Start of skeleton fix for floor poses

---
+++ Assets/Scripts/JointScripts/SkeletonRootPositioner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkeletonRootPositioner : MonoBehaviour
+{
+    GameObject rootNode = null;
+    private Transform rootStartTransform;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rootStartTransform = rootNode.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Modify all joints based on difference between startpos and current pos
+        foreach (JointGizmo joint in rootNode.GetComponentsInChildren<JointGizmo>()) {
+            
+        }
+    }
+}
