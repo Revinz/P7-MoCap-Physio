2efcc1f571749610b045ce13d2a05011aa5f406d
Revinz <revinz1405@gmail.com>
2020-10-13 09:26:13 +0200

    Restructured + added WIP Joint class

--- Assets/Scripts/JointScripts/JointGizmo.cs
+++ Assets/Scripts/JointScripts/JointGizmo.cs
@@ -5,20 +5,23 @@ using UnityEngine;
 [ExecuteInEditMode]
 public class JointGizmo : MonoBehaviour
 {
+    #region Fields
     public float gizmoSphereSize = 0.07f;
     public float gizmoLineLength = 0.2f;
     public Vector3 rotOffset = Vector3.zero;
     private Vector3 pos;
     private Vector3 rot;
+    #endregion
 
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.green;
+
         //Location
         Gizmos.DrawSphere(gameObject.transform.position, gizmoSphereSize);
 
         //Joint facing direction
-        Gizmos.DrawLine(gameObject.transform.position, gameObject.transform.position + gameObject.transform.forward.normalized * gizmoLineLength);
-
+        Gizmos.DrawLine(gameObject.transform.position,
+            gameObject.transform.position + gameObject.transform.forward.normalized * gizmoLineLength);
     }
 }
