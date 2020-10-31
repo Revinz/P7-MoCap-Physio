53a3c6054694c3f2219f15b45d6a6094fa879c69
Revinz <revinz1405@gmail.com>
2020-10-31 10:54:56 +0100

    Added and configured AsyncStorage

--- MobileApp/App.js
+++ MobileApp/App.js
@@ -1,6 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+//To test if AsyncStorage is working
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export default function App() {
   return (
@@ -14,8 +16,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
