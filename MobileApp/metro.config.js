0a52ae5465bd6b0320a47eafd856e1a9e924699b
Revinz <revinz1405@gmail.com>
2020-10-31 12:02:44 +0100

    Now the BundleResourceIO should work. Messed up before.

--- MobileApp/metro.config.js
+++ MobileApp/metro.config.js
@@ -3,9 +3,6 @@ module.exports = (async () => {
   const defaultConfig = await getDefaultConfig();
   const { assetExts } = defaultConfig.resolver;
   return {
-    transformer: {
-      assetPlugins: ["expo-asset/tools/hashAssetFiles"],
-    },
     resolver: {
       // Add bin to assetExts
       assetExts: [...assetExts, "bin"],
