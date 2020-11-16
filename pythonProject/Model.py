7bf378e92b38f197cee30bf67c8f63a9675a7143
Mikkel Gede Hansen <mgh7654321@gmail.com>
2020-11-16 18:00:42 +0100

    2 failed sequencing attempts

--- pythonProject/Model.py
+++ pythonProject/Model.py
@@ -14,10 +14,15 @@ import pandas as pd
 
 # Data preparation
 data = pd.read_csv('PoseDataset.csv')  # Reads the file & sorts data into columns
-for i in range(6):
-    print("DATA FOR ROW", i, data.iloc[i, 18])  # Prints row 0-6, column 18 to see if it matches excel
+#for i in range(6):
+#    print("DATA FOR ROW", i, data.iloc[i, 18])  # Prints row 0-6, column 18 to see if it matches excel
 
-print("data: ", data)
+#-------------ATTEMPT 2, Prints out the same data, no way to see if its actually seperated into time series-------------
+# https://stackabuse.com/solving-sequence-problems-with-lstm-in-keras/
+# data.values.reshape(-1,5,1)
+# for i in range(6):
+#     print("data: ", data.iloc[i,1])
+#--------------------------------------------------------END OF ATTEMPT------------------------------------------------
 
 target = data["exercise"]  # Extracts only the target for later prediction
 print("target: ", target)
@@ -25,6 +30,29 @@ print("target: ", target)
 data.drop(["exercise"], inplace=True, axis=1)  # Removes both exercise & filepath columns.
 data.drop(["File Path"], inplace=True, axis=1)
 print("sorted data:  ", data)
+
+#-----------ATTEMPT 1, Works with target but not the 17 body joint samples--------------------------------------------
+#https://machinelearningmastery.com/how-to-develop-lstm-models-for-time-series-forecasting/
+# split a univariate sequence into samples -ISSUE: 17 body joint variables are NOT univariate.
+# def split_sequence(sequence, n_steps):
+#     X = data
+#     y = target
+#     X, y = list(), list()
+#     for i in range(len(sequence)):
+#         # find the end of this pattern
+#         end_ix = i + n_steps
+#         # check if we are beyond the sequence
+#         if end_ix > len(sequence)-1:
+#             break
+#         # gather input and output parts of the pattern
+#         seq_x, seq_y = sequence[i:end_ix], sequence[end_ix]
+#         X.append(seq_x)
+#         y.append(seq_y)
+#     return list(X), list(y)
+#
+# X, y = split_sequence(data, 5)
+# print(X, y)
+#------------------------------------END OF ATTEMPT-------------------------
 
 # Split data into test and training
 #x_train, x_test, y_train, y_test = train_test_split(data, target, test_size=0.2, random_state=4)
