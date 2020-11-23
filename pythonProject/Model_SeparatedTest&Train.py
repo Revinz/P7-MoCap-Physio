55058856fd87423d24dbab51f51e19eb5d7da440
RasmusJoergensen <31987814+RasmusJoergensen@users.noreply.github.com>
2020-11-23 12:20:16 +0100

    Added model summary plots

--- pythonProject/Model_SeparatedTest&Train.py
+++ pythonProject/Model_SeparatedTest&Train.py
@@ -218,7 +218,20 @@ print("Fit time: %0.2f seconds" % (time.time() - start_time))
 
 results = model.predict(x_test, verbose=0)
 np.set_printoptions(suppress=True)
+plt.plot(history.history['accuracy'])
+plt.plot(history.history['val_accuracy'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
+# summarize history for loss
 plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
 plt.show()
 # -------------------------------------------------------------------------------
 # PREDICTION
